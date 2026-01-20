d94108280eb51c10bfbeff0c47e3fd8bb8355268
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-01-20 20:51:32 -0300

    add Category entity with fields and validation

--- src/main/java/br/com/moneyflow/model/entity/Category.java
+++ src/main/java/br/com/moneyflow/model/entity/Category.java
@@ -1,4 +1,39 @@
 package br.com.moneyflow.model.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "Category")
+@Table(name="categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Column(nullable = false, length = 80)
+    private String name;
+    @Column(length = 200)
+    private String description;
+    @Enumerated(EnumType.STRING)
+    private CategoryType type;
+    @NotBlank
+    private String color;
+    @NotBlank
+    private String icon;
+    private Long userId;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
 }
--- src/main/java/br/com/moneyflow/model/entity/User.java
+++ src/main/java/br/com/moneyflow/model/entity/User.java
@@ -3,9 +3,7 @@ package br.com.moneyflow.model.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,16 +21,13 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank
     @Column(nullable = false, length = 100)
     private String name;
-
     @NotBlank
     @Email
     @Column(nullable = false, length = 120)
     private String email;
-
     @NotBlank
     @Column(nullable = false)
     private String password;
