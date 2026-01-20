d72b1b6e72b18d8873192f40032d23827bcc7546
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-01-20 20:13:36 -0300

    First Commit

---
+++ src/main/java/br/com/moneyflow/model/entity/Category.java
@@ -0,0 +1,4 @@
+package br.com.moneyflow.model.entity;
+
+public class Category {
+}
---
+++ src/test/java/br/com/moneyflow/MoneyFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moneyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
