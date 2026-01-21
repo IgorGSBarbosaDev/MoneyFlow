577a341e481319683a24da7156afd039399c32bb
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-01-21 19:26:44 -0300

    refactor User entity: enhance validation, add JSON property handling, and improve equality/hashcode methods

--- src/main/java/br/com/moneyflow/model/entity/User.java
+++ src/main/java/br/com/moneyflow/model/entity/User.java
@@ -1,40 +1,44 @@
 package br.com.moneyflow.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
-
-
-
 
 @Entity(name = "User")
 @Table(name = "users")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(exclude = "password")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
+    @Setter
     @NotBlank
     @Column(nullable = false, length = 100)
     private String name;
     @NotBlank
+    @Setter
     @Email
     @Column(nullable = false, length = 120)
     private String email;
     @NotBlank
+    @Setter
     @Column(nullable = false)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
     @Column(nullable = false)
     private LocalDateTime updatedAt;
+    @Setter
     @Column(nullable = false)
     private Boolean active;
 
