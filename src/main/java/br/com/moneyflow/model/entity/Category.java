0794c9ed5a6c5f7fb3e7fefa51298c3688494ace
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-01-21 19:28:43 -0300

    refactor Category entity: remove unnecessary import statement

--- src/main/java/br/com/moneyflow/model/entity/Category.java
+++ src/main/java/br/com/moneyflow/model/entity/Category.java
@@ -3,7 +3,6 @@ package br.com.moneyflow.model.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
-
 import java.time.LocalDateTime;
 
 @Entity(name = "Category")
