e421d06795dca28085d214728707ea52721a0478
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-01-21 18:51:51 -0300

    refactor Category entity: enhance validation, add user relationship

--- src/main/java/br/com/moneyflow/model/entity/Category.java
+++ src/main/java/br/com/moneyflow/model/entity/Category.java
@@ -2,10 +2,7 @@ package br.com.moneyflow.model.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
@@ -15,9 +12,11 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
     @NotBlank
     @Column(nullable = false, length = 80)
@@ -27,13 +26,27 @@ public class Category {
     @Enumerated(EnumType.STRING)
     private CategoryType type;
     @NotBlank
+    @Column(nullable = false, length = 20)
     private String color;
     @NotBlank
+    @Column(nullable = false, length = 200)
     private String icon;
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn( name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_category_user"))
+    private User userId;
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
     @Column(nullable = false)
     private LocalDateTime updatedAt;
 
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
