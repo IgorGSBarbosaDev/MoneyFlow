78078153dad6d085ad8afe73211c0482ae3b1b01
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-01-22 20:42:21 -0300

    feat: add AlertLevel and AlertType enums for alert categorization

---
+++ src/main/java/br/com/moneyflow/model/entity/AlertLevel.java
@@ -0,0 +1,7 @@
+package br.com.moneyflow.model.entity;
+
+public enum AlertLevel {
+    INFO,
+    WARNING,
+    CRITICAL
+}
---
+++ src/main/java/br/com/moneyflow/model/entity/AlertType.java
@@ -0,0 +1,8 @@
+package br.com.moneyflow.model.entity;
+
+public enum AlertType {
+    BUDGET_WARNING,
+    BUDGET_EXCEEDED,
+    BUDGET_CREATED,
+    MONTHLY_SUMMARY
+}
