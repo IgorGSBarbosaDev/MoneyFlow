5376dc60cfc6f3e340f59cda241a274d022dcd64
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-01-23 11:04:58 -0300

    refactor: simplify UserRepository methods and update query for user creation dates

--- src/main/java/br/com/moneyflow/repository/UserRepository.java
+++ src/main/java/br/com/moneyflow/repository/UserRepository.java
@@ -2,7 +2,6 @@ package br.com.moneyflow.repository;
 
 import br.com.moneyflow.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -10,13 +9,10 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
-    Boolean existsByEmail(String email);
+    boolean existsByEmail(String email);
     Optional<User> findByEmailAndActive(String email, boolean active);
     List<User> findAllByActive(boolean active);
     long countByActiveTrue();
     Optional<User> findByEmailIgnoreCase(String email);
-
-    @Query("SELECT u FROM User u WHERE u.createdAt BETWEEN :startDate AND :endDate")
-    List<User>findAllUsersCreatedBetweenDates(LocalDateTime starDate, LocalDateTime endDate);
-
+    List<User> findAllByCreatedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
 }
