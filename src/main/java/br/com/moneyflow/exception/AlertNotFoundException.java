0185d701d677a23b222eef4e6a759d98c754cafb
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-02-05 19:41:01 -0300

    feat: create AlertNotFoundException for handling alert not found scenarios

---
+++ src/main/java/br/com/moneyflow/exception/AlertNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.moneyflow.exception;
+
+public class AlertNotFoundException extends RuntimeException {
+    public AlertNotFoundException(String message) {
+        super(message);
+    }
+}
