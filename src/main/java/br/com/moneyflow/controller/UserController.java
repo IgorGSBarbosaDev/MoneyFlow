7248df292d83f2417abc10af23c07035c6819897
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-02-06 19:34:57 -0300

    feat: add UserController with endpoint for user creation

---
+++ src/main/java/br/com/moneyflow/controller/UserController.java
@@ -0,0 +1,23 @@
+package br.com.moneyflow.controller;
+
+import br.com.moneyflow.model.entity.User;
+import br.com.moneyflow.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser() {
+
+        return null;
+    }
+}
