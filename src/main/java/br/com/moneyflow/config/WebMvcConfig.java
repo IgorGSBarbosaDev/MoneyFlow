fe40de09ee6069f1f9770b59a795ffe28628a03e
Igor Barbosa <igorgsbarbosadev@gmail.com>
2026-02-07 11:20:47 -0300

    feat: add CORS configuration for API endpoints to allow cross-origin requests

--- src/main/java/br/com/moneyflow/config/WebMvcConfig.java
+++ src/main/java/br/com/moneyflow/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import br.com.moneyflow.config.security.CurrentUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,19 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:4200",
+                        "http://localhost:8080"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
